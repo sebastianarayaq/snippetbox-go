755c7d02d0945dc150af846623623c49911d8542
sarayaq <araya.sebastian.andres@gmail.com>
2024-08-09 03:18:35 -0400

    Add HTML templates, static assets and update handlers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,9 +9,14 @@ func main() {
 	loadEnv()
 
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+
 	log.Printf("Starting server on %s", PORT)
 	err := http.ListenAndServe(PORT, mux)
 	log.Fatal(err)
